9e2206059c57fe3fe055456aca3ddba8e7670568
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-08-18 10:17:35 +0100

    docs(pt): translate `routes/examples/[slug]/+page.svelte`

--- sites/svelte.dev/src/routes/examples/[slug]/+page.svelte
+++ sites/svelte.dev/src/routes/examples/[slug]/+page.svelte
@@ -30,14 +30,14 @@
 </script>
 
 <svelte:head>
-	<title>{data.example?.title} {data.example?.title ? '•' : ''} Svelte Examples</title>
+	<title>{data.example?.title} {data.example?.title ? '•' : ''} Exemplos de Svelte</title>
 
-	<meta name="twitter:title" content="Svelte examples" />
-	<meta name="twitter:description" content="Cybernetically enhanced web apps" />
-	<meta name="Description" content="Interactive example Svelte apps" />
+	<meta name="twitter:title" content="Exemplos de Svelte" />
+	<meta name="twitter:description" content="Aplicações de Web Ciberneticamente Melhoradas" />
+	<meta name="Description" content="Exemplos Interativos de Aplicações de Svelte" />
 </svelte:head>
 
-<h1 class="visually-hidden">Examples</h1>
+<h1 class="visually-hidden">Exemplos</h1>
 <div class="examples-container" bind:clientWidth={width}>
 	<div class="viewport offset-{offset}">
 		<TableOfContents
