eec555df39954d5faa21cccc8e65ee4d87f1e665
Nazaré da Piedade <nazarepiedady@gmail.com>
2024-01-22 13:10:03 +0100

    chore: update the try component

--- sites/svelte.dev/src/routes/_components/Try.svelte
+++ sites/svelte.dev/src/routes/_components/Try.svelte
@@ -7,9 +7,9 @@
 	<Section --background="var(--background-2)">
 		<div class="grid" style="--columns: 2">
 			<div class="copy">
-				<h2>vê</h2>
+				<h2>vê com os teus próprios olhos</h2>
 				<div>
-					Testar localmente, <a target="_blank" rel="noreferrer" href="https://sveltekit.new">na StackBlitz</a>, ou com <a target="_blank" href="https://learn-svelte-pt.vercel.app">o tutorial</a>.
+					Testa localmente, <a target="_blank" rel="noreferrer" href="https://sveltekit.new">na StackBlitz</a>, ou com <a target="_blank" href="https://learn-svelte-pt.vercel.app">o tutorial</a>.
 				</div>
 			</div>
 
